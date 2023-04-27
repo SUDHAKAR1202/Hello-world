9d8e8be4cec5272e8feebb6c0cfd890debc34647
SUDHAKAR1202 <sudzcrazy94@gmail.com>
2023-04-27 17:39:42 +0530

    added object destructuring and read  query params

--- app.js
+++ app.js
@@ -7,6 +7,27 @@ app.get('/', (req,res)=>{
     res.send('Hello World!');
 })
 
+app.get('/user', function(req,res){
+   var fruit = req.query.fruit
+ 
+  console.log("Fruit :", fruit);
+   
+})
+
+app.get('/details' , function(req,res) {
+ let employee_details = ["Sudhakar", 1200, "Software Engineer"];
+
+let getdetails = ([name_of_an_employee, employee_id, employee_designation]) => {
+    console.log(`Employee's name: ${name_of_an_employee}, Employee's id: ${employee_id}, Employee's designation: ${employee_designation}`
+    );
+
+  };
+  getdetails(employee_details);
+})
+    
+    
+
+
 app.listen(port, ()=>{
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Server listening on port ${port}`)
 })
